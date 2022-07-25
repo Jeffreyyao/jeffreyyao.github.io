7fdc6f30054e6865cdb513d1b8bc3e2584469714
Jeffreyyao <jeffreyyao@outlook.com>
2022-07-25 02:24:01 -0600

    update

--- src/footer.js
+++ src/footer.js
@@ -3,7 +3,7 @@ footer.innerHTML = `
     <br><br>
     <footer>
         <p style="text-align:left;">
-            All Work © 2022 JJ
+            All Work © 2022 JJ 🐲 Built with <a href="https://watercss.kognise.dev">Water.css</a>
             <span style="float:right;">
             <a href="#">Back to top ⬆</a>
             </span>
